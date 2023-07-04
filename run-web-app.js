abfcbb4f7e8ffd292c4ce9e101b7130f38219b98
Timur Petin <timur560@gmail.com>
2023-07-04 11:36:01 +0200

    Add ssl staff

--- run-web-app.js
+++ run-web-app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require('http');
+const fs = require('fs');
+const https = require('https');
 require('dotenv').config({ path: `${__dirname}/.env` });
 
-const BUILD_PATH = '/home/ubuntu/go_connect_web/build';
+const BUILD_PATH = '/home/ubuntu/opf_admin/build';
+
+const options = {
+  key: fs.readFileSync(process.env.KEY_PATH),
+  cert: fs.readFileSync(process.env.CERT_PATH),
+};
 
 app.use(express.static(BUILD_PATH));
 
@@ -11,6 +17,6 @@ app.all('*', function (req, res) {
   res.sendFile(`${BUILD_PATH}/index.html`);
 });
 
-http.createServer(app).listen(process.env.PORT, () => {
+https.createServer(options, app).listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
